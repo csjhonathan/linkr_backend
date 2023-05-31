1eef3bcf3175b25fce6494282e382fd6e3e4c0be
csjhonathan <jhonathancarv.s@gmail.com>
2023-05-31 13:44:44 -0300

    feat: the route for sign in is ready with the proper validations and sanitizations, returning an access token if the user was able to sign in successfully

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import db from '../database/connection.js';
+import Jwt from 'jsonwebtoken';
 import * as usersRepository from '../repositories/users.repositories.js';
 
 export async function signUp(req, res) {
@@ -7,9 +7,23 @@ export async function signUp(req, res) {
 
   try {
     schema.password = bcrypt.hashSync(schema.password, 10);
+
     await usersRepository.create(schema);
+
     return res.status(201).send({ message: 'User has been registered!' });
   } catch (error) {
     return res.status(500).send({ message: error.message });
   }
+}
+
+export async function signIn(req, res) {
+  const { user, schema } = res.locals;
+  const passwordIsCorrect = bcrypt.compareSync(schema.password, user.password);
+
+  if (!passwordIsCorrect) return res.status(401).send({ message: 'Incorrect password!' });
+
+  const payload = { id: user.id, name: user.name, email: user.email };
+  const token = Jwt.sign(payload, process.env.SECRET_KEY);
+
+  return res.status(200).send({ token });
 }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { signUp } from '../controllers/users.controllers.js';
+import { signUp, signIn } from '../controllers/users.controllers.js';
 import * as schema from '../schemas/user.schemas.js';
 import schemaValidator from '../middlewares/schemaValidator.middleware.js';
-import userVerifierExists from '../middlewares/userVerifierExists.js';
+import userVerifierExists from '../middlewares/userVerifierExists.middleware.js';
 
 const usersRoutes = Router();
 
 usersRoutes.post('/signup', schemaValidator(schema.signUp), userVerifierExists, signUp);
+usersRoutes.post('/signin', schemaValidator(schema.signIn), userVerifierExists, signIn);
 
 export default usersRoutes;

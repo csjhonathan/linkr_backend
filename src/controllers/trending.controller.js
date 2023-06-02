bb5439ce3edb0936cda173ea1af495dfb55c691d
Felipe Pires Iasbik <felipeiasbik@hotmail.com>
2023-06-02 18:31:36 -0300

    fix: hashtag select by name

--- src/controllers/trending.controller.js
+++ src/controllers/trending.controller.js
@@ -7,12 +7,14 @@ export async function listTrending(req, res) {
   const token = authorization?.replace('Bearer ', '');
   if (!token) return res.sendStatus(401);
   try {
-    const invalidToken = await listInvalidToken(authorization);
+    const invalidToken = await listInvalidToken(token);
     if (invalidToken.rowCount !== 0) return res.status(401).send('User not allowed access.');
-    Jwt.verify(token, process.env.SECRET_KEY);
+    // Jwt.verify(token, process.env.SECRET_KEY);
     const listTrending = await getTrending();
     res.send(listTrending.rows);
   } catch (err) {
+    if (err.name === 'TokenExpiredError') res.status(500).send('Token expirado');
+    if (err.name === 'JsonWebTokenError') res.status(500).send('Token inválido');
     res.status(500).send(err.message);
   }
 }
@@ -23,12 +25,14 @@ export async function listHashtagById(req, res) {
   const token = authorization?.replace('Bearer ', '');
   if (!token) return res.sendStatus(401);
   try {
-    const invalidToken = await listInvalidToken(authorization);
+    const invalidToken = await listInvalidToken(token);
     if (invalidToken.rowCount !== 0) return res.status(401).send('User not allowed access.');
-    Jwt.verify(token, process.env.SECRET_KEY);
+    // Jwt.verify(token, process.env.SECRET_KEY);
     const postsByHashtag = await hashtagById(hashtag);
     res.send(postsByHashtag.rows);
   } catch (err) {
+    if (err.name === 'TokenExpiredError') res.status(500).send('Token expirado');
+    if (err.name === 'JsonWebTokenError') res.status(500).send('Token inválido');
     res.status(500).send(err.message);
   }
 }

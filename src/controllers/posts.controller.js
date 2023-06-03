65df071df5a8dd9a628e98460501d9d4e75f2dc1
filipetenedini <tenedinifilipe@gmail.com>
2023-06-03 10:56:04 -0300

    (feat) list posts

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -2,13 +2,14 @@ import postsRepository from '../repositories/posts.repository.js';
 import tagsRepository from '../repositories/tags.repository.js';
 
 async function create(req, res) {
-  const { schema } = res.locals;
+  const { schema, user } = res.locals;
   const {
-    url, description, userId, createdAt,
+    url, description, createdAt,
   } = schema;
   const regex = /#\w{1,}/g;
   const hashtagList = description && description.match(regex);
-  const values = description ? [url, description, userId, createdAt] : [url, userId, createdAt];
+  const values = description ? [url, description, user.id, createdAt] : [url, user.id, createdAt];
+
   try {
     const post = await postsRepository.createPost(values);
 
@@ -20,4 +21,26 @@ async function create(req, res) {
     return res.status(500).send({ message: err.message });
   }
 }
-export default { create };
+
+async function listUserTimeline(req, res) {
+  const { id } = req.params;
+  try {
+    const timelinePosts = await postsRepository.listUserPosts(id);
+
+    return res.status(200).send(timelinePosts);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+}
+
+async function listTimelinePosts(req, res) {
+  try {
+    const timelinePosts = await postsRepository.listPosts();
+
+    return res.status(200).send(timelinePosts);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+}
+
+export default { create, listTimelinePosts, listUserTimeline };

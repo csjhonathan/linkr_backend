f8bcc941e7c070eaaf4fd36895401cecc7ecee7a
csjhonathan <jhonathancarv.s@gmail.com>
2023-06-06 17:26:43 -0300

    fix: adding the field followingUser when search user

--- src/controllers/search.controllers.js
+++ src/controllers/search.controllers.js
@@ -1,19 +1,13 @@
-import Jwt from 'jsonwebtoken';
-import { listInvalidToken } from '../repositories/blacklist.repositories.js';
-import { searching } from '../repositories/search.repository.js';
+import searching from '../repositories/search.repository.js';
 
-export async function searchUsers(req, res) {
-  const { authorization } = req.headers;
+export default async function searchUsers(req, res) {
   const { name } = req.query;
-  const token = authorization?.replace('Bearer ', '');
-  if (!token) return res.sendStatus(401);
+  const { id } = res.locals.user;
   try {
-    const invalidToken = await listInvalidToken(token);
-    if (invalidToken.rowCount !== 0) return res.status(401).send('User not allowed access.');
-    // Jwt.verify(token, process.env.SECRET_KEY);
-    const listSearch = await searching(name);
+    const listSearch = await searching(name, id);
     res.send(listSearch.rows);
   } catch (err) {
+    console.log(err);
     res.status(500).send(err.message);
   }
 }
